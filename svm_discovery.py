9bf8de1ee3b1d32354feeb7bd27a2b6ff0848cb1
pegahottawa <62723000+pegahottawa@users.noreply.github.com>
2020-07-22 12:27:53 -0500

    Update svm_discovery.py

--- svm_discovery.py
+++ svm_discovery.py
@@ -34,29 +34,28 @@ df2 = pd.read_excel(path_to_file)
 df3 = df2.transpose()
 
 path_to_file = '/home/pegah/project_discovery/column.xlsx'  # column.xlsx file contain only column names for about 48804 gene names
-df100 = pd.read_excel(path_to_file)
+column_names = pd.read_excel(path_to_file)
 
-df3.columns = df3.iloc[0]  # after trasposing a df2 column names become the first row ,thats why I renamed the column names with the firts row
-df4 = df3.drop(df3.index[[0]])     # after that because by renaming I would end up with same
-                                   # column names and and same row values,I droped the first row
+df3.columns = df3.iloc[0] 
+df4 = df3.drop(df3.index[[0]])     
 
-df6=df4.reset_index()              # I wanted to have dataframe format with all index for each row
-df6.columns = df100.iloc[:,0]      # After all, now its time to rename columns
-result = pd.merge(df1,df6,on='METABRIC_ID')
+df6=df4.reset_index()             
+df6.columns = df100.iloc[:,0]      
+df_merge = pd.merge(df1,df6,on='METABRIC_ID')
 
-del_NON = result.dropna(how = 'any')  # drop row if you find any NON value in it
+del_NON = df_merge.dropna(how = 'any')  # drop rows wuth NON value in it
 
-Tar = del_NON[del_NON.columns[6]]
+Target = del_NON[del_NON.columns[6]]
 Descriptor = del_NON.drop(del_NON.columns[[0,1,2,6]], axis=1)
 arr_descriptors=Descriptor.values
-arr_Target=Tar.values
+arr_Target=Target.values
 
 
 X = arr_descriptors
 T = arr_Target
 #Target = np.reshape(T, (609,1)) # when not using Kfold cv and using Holdout cv method
-Target = np.reshape(T, (533,)) # when using kfold cross validation you need this sort of reshape
-Y = Target
+Target_reshape = np.reshape(Target, (533,)) 
+Y = Target_reshape
 
 
 print 'X.shape =', X.shape
@@ -81,5 +80,5 @@ scores = cross_val_score( reg, X_train, y_train, cv=5)
 print " result for svmcleand "
 print scores
 print("Average 4-Fold CV Score: {}".format(np.mean(scores)))
-print "svm"
+
 
