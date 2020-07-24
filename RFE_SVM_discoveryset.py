b65182853eb3019d4b8a4269a5c37d451e2033bd
pegahottawa <62723000+pegahottawa@users.noreply.github.com>
2020-07-23 22:02:58 -0500

    Update and rename RFE_discovery_svmcleaned.py to RFE_SVM_discoveryset.py

--- RFE_SVM_discoveryset.py
+++ RFE_SVM_discoveryset.py
@@ -24,42 +24,40 @@ df2 = pd.read_excel(path_to_file)
 df3 = df2.transpose()
 
 path_to_file = '/home/peg/column.xlsx'  # column.xlsx file contain only column names for about 48804 gene names
-df100 = pd.read_excel(path_to_file)
+column_names = pd.read_excel(path_to_file)
 
-df3.columns = df3.iloc[0]  # after trasposing a df2 column names become the first row ,thats why I renamed the column names
-                           # with the firts row
-df4 = df3.drop(df3.index[[0]])     # after that because by renaming I would end up with same
-                                   # column names and and same row values,I droped the first row
+df3.columns = df3.iloc[0]  
+df4 = df3.drop(df3.index[[0]])     
 
-df6=df4.reset_index()              # I wanted to have dataframe format with all index for each row
-df6.columns = df100.iloc[:,0]      # After all, now its time to rename columns
-result = pd.merge(df1,df6,on='METABRIC_ID')
+df6 = df4.reset_index()              
+df6.columns = column_names.iloc[:,0]      
+df_merge = pd.merge(df1,df6,on='METABRIC_ID')
 
-NON_value = result.isnull().sum()   # how many NON value we have in each column
+NON_value = df_merge.isnull().sum()   
 #print NON_value
-del_NON = result.dropna(how = 'any')  # drop row if you find any NON value in it
+del_NON = df_merge.dropna(how = 'any') 
 #print del_NON
 
-Tar = del_NON[del_NON.columns[6]]
+Target = del_NON[del_NON.columns[6]]
 #print Tar
 Descriptor = del_NON.drop(del_NON.columns[[0,2,6]], axis=1)
-arr_descriptors=Descriptor.values
-arr_Target=Tar.values
+arr_descriptors = Descriptor.values
+arr_Target = Target.values
 
 feature_lst = list(Descriptor.columns.values)
 
 X = arr_descriptors
 T = arr_Target
-Target = np.reshape(T, (554,)) # when using kfold cross validation you need this sort of reshape
+Targe_reshape = np.reshape(T, (554,)) 
 #Target = np.reshape(T, (554,1))
-Y = Target
+Y = Target_reshape
 
 print 'X.shape =', X.shape
 print 'Y.shape =', Y.shape
 
-##############################################
-#SVM
-##############################################
+#######################################
+# RFE with SVM and Cross_validation
+#######################################
 
 X_train,X_test,y_train,y_test=train_test_split(X, Y, random_state=0)
 
@@ -89,8 +87,6 @@ X_test_rfecv = rfecv.transform(X_test)
 
 rfecv_model = reg.fit(X_train_rfecv ,y_train)
 y_pred=reg.predict(X_test_rfecv)
-
-print "Final result svm discovery cleaned"
 
 print(reg.score(X_test_rfecv, y_test))
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
